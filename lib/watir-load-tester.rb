767c36a1b9c120989dd040f84c1ad61b1614cdcc
Mitch Beaumont <mitch.beaumont@gmail.com>
2016-04-02 22:54:13 +1100

    Refactored to use optparse

--- lib/watir-load-tester.rb
+++ lib/watir-load-tester.rb
@@ -1,4 +1,5 @@
 require 'thread'
+require 'optparse'
 # Main watir-load-tester class
 class WatirLoadTester
   # Load Testing Tool to allow you to utilize Watir-Classic's IE.new_process
@@ -9,41 +10,43 @@ class WatirLoadTester
   #     5              - How many users you are simulating
   #     1              - The delay between users
   #     c:\results.log - Where I want my output
-  def self.load_test(testScript,num_itterations=1,num_users=1,delay_between_users=0, log_file_name = Dir.pwd + '/load_test.log')
+
+  #def self.load_test(testScript,num_itterations=1,num_users=1,delay_between_users=0, log_file_name = Dir.pwd + '/load_test.log')
+  def self.load_test(myoptions) # Method invoked using a hash of options which allows for named arguments and variable argument lengths. (https://en.wikibooks.org/wiki/Ruby_Programming/Syntax/Method_Calls#Variable_Length_Argument_List.2C_Asterisk_Operator
     total_passing = 0
     total_failing = 0
-    log = File.open(log_file_name,"a")
+    log = File.open(myoptions[:log_file_name],"a")
 
-    log.puts("\n\n*********************************\n**Load Test Configuration: num_itterations=#{num_itterations}, num_users=#{num_users}, delay_between_users=#{delay_between_users} seconds.")
+    log.puts("\n\n*********************************\n**Load Test Configuration: num_itterations=#{myoptions[:num_itterations]}, num_users=#{myoptions[:num_users]}, delay_between_users=#{myoptions[:delay_between_users]} seconds.")
 
-    (1..num_itterations).each do |iteration|
+    (1..myoptions[:num_itterations]).each do |iteration|
       itteration_passing = 0
       itteration_failing = 0
       log.puts("**Begining Itteration #{iteration} - #{Time.now.strftime(" %m/%d/%y @ %H:%M:%S ") }")
       i=0
       threads = []
-      num_users.times  do  #each thread simulates a user running the specified testScript
+      myoptions[:num_users].times  do  #each thread simulates a user running the specified testScript
           threads << Thread.new do
             i=i+1
             startTime = Time.now
-            result = system("ruby #{testScript}")  #execute the script and wait for it to finish.
+            result = system("ruby #{myoptions[:testScript]}")  #execute the script and wait for it to finish.
             duration = Time.now-startTime
             if result  #Record our results.
               itteration_passing = itteration_passing +1
-              log.puts("Thread #{i} executed #{testScript} Successfully - Duration = #{duration}seconds")
+              log.puts("Thread #{i} executed #{myoptions[:testScript]} Successfully - Duration = #{duration}seconds")
             else
               itteration_failing = itteration_failing + 1
-              log.puts("Thread #{i} executed #{testScript} FAILED - Duration = #{duration}seconds")
+              log.puts("Thread #{i} executed #{myoptions[:testScript]} FAILED - Duration = #{duration}seconds")
             end
           end
-        sleep(delay_between_users)  #Wait for the specified delay_between_users before we start the next user/thread.
+        sleep(myoptions[:delay_between_users])  #Wait for the specified delay_between_users before we start the next user/thread.
       end
       threads.each {|t| t.join}
-      log.puts("**Finished Itteration #{iteration}. #{itteration_passing} out of #{num_users} tests Passed - #{Time.now.strftime(" %m/%d/%y @ %H:%M:%S ") }")
+      log.puts("**Finished Itteration #{iteration}. #{itteration_passing} out of #{myoptions[:num_users]} tests Passed - #{Time.now.strftime(" %m/%d/%y @ %H:%M:%S ") }")
       total_passing = total_passing + itteration_passing
       total_failing = total_failing + itteration_failing
     end
-    log.puts("**Load Test complete.  #{total_passing} out of #{num_users*num_itterations} tests Passed")
+    log.puts("**Load Test complete.  #{total_passing} out of #{myoptions[:num_users]*myoptions[:num_itterations]} tests Passed")
     log.close
   end
 end
